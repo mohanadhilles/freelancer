07560fc6cd7799f8d82f5bcf46dcb1b295545c6d
Mohanad <mohanad@Mohanads-MacBook-Pro.local>
2021-02-12 04:43:35 +0200

    add zoho

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -117,17 +117,7 @@
             jQuery(".pins").delay(500).fadeOut("slow");
         });
 	</script>
-	<script type="text/javascript">
-		var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-		(function(){
-		var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-		s1.async=true;
-		s1.src='https://embed.tawk.to/5e0db55e27773e0d832b7e13/1ekjnltcj';
-		s1.charset='UTF-8';
-		s1.setAttribute('crossorigin','*');
-		s0.parentNode.insertBefore(s1,s0);
-		})();
-		</script>
+
 		<!--End of Tawk.to Script-->
 		<!-- Global site tag (gtag.js) - Google Analytics -->
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-168595512-1"></script>
