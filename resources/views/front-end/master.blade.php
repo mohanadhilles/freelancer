7bb1a0c237df97823509a53a833cae297bd5e0d2
Mohanad <mohanad@Mohanads-MacBook-Pro.local>
2021-02-12 04:36:50 +0200

    add zoho

--- resources/views/front-end/master.blade.php
+++ resources/views/front-end/master.blade.php
@@ -116,5 +116,8 @@
 		}
 	});
 </script>
+<script type="text/javascript">
+var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"76e552bf93352cd3f332fe1b6ef5b5b60dacb0f109db044c25b6ef720716b861", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+</script>
 @stack('scripts')
 @endpush
